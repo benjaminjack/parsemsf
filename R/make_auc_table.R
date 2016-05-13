9350fd0d5c8258cd7eea5deab85c30e94d910656
Benjamin Jack <benjamin.r.jack@gmail.com>
2016-05-13 17:46:05 -0500

    Documentation for all functions is now complete.

--- R/make_auc_table.R
+++ R/make_auc_table.R
@@ -1,70 +1,70 @@
 
 #' Make a table of peptide areas
 #'
-#' @param msf_file 
-#' @param min_conf 
-#' @param as_df 
+#' @param msf_file A file path to a thermo MSF file.
+#' @param min_conf "High", "Medium", or "Low". The mininum peptide confidence level to retrieve from MSF file.
 #'
-#' @return
+#' @return A data frame containing peptide areas for peptides at or above the minimum confidence level.
 #' @export
 #'
 #' @examples
-make_auc_table <- function(msf_file, min_conf = "High", as_df = TRUE) {
-  
+#' make_auc_table("mythermofile.msf")
+make_auc_table <- function(msf_file, min_conf = "High") {
+
   # Access MSF database file
   my_db <- src_sqlite(msf_file)
-  
+
   Events <- tbl(my_db, "Events") %>%
     select(-RT, -LeftRT, -RightRT, -SN, -FileID, -Intensity)
-  
+
   EventAreaAnnotations <- tbl(my_db, "EventAreaAnnotations") %>%
     select(EventID, QuanResultID)
-  
+
   PrecursorIonAreaSearchSpectra <- tbl(my_db, "PrecursorIonAreaSearchSpectra")
-  
+
   # Here is where all the scan information, including mass and charge
   SpectrumHeaders <- tbl(my_db, "SpectrumHeaders") %>%
     select(SpectrumID, FirstScan, Mass, Charge, RetentionTime, UniqueSpectrumID) %>%
     collect()
-  
+
   # Grab intensities
   MassPeaks <- tbl(my_db, "MassPeaks") %>%
     select(MassPeakID, Intensity) %>%
     collect()
-  
+
   # Here are all the areas
   # m/z is really just one of many m/z's that could be retrieved from the msf file.
   # All of the m/z's are calculated slightly differently. I just picked one since
   # I don't use m/z's for my work.
-  events_joined <- inner_join(Events, EventAreaAnnotations, by = "EventID") %>% 
+  events_joined <- inner_join(Events, EventAreaAnnotations, by = "EventID") %>%
     inner_join(PrecursorIonAreaSearchSpectra, by = "QuanResultID") %>%
     collect()  %>%
     group_by(SearchSpectrumID) %>%
     summarize(Area = sum(Area), m_z = mean(Mass))
-  
+
   # NOTE: mass and m/z are probably not correct right now! I have check them in more detail
-  
+
   # Join areas to the spectrum IDs. Not all spectrum IDs have areas!
   # I haven't figured out what the difference between UniqueSpectrumID and SpectrumID are,
   # except that MassPeakID's correspond to UniqueSpectrumID ONLY.
   spectra <- left_join(SpectrumHeaders, events_joined, by = c("SpectrumID" = "SearchSpectrumID")) %>%
     inner_join(MassPeaks, by = c("UniqueSpectrumID" = "MassPeakID"))
-  
+
   pep_table <- make_pep_table(msf_file, min_conf)
-  
+
   # Join peptide info to mass/area/charge/etc.
-  auc_table <- right_join(spectra, pep_table, by=c("SpectrumID" = "SpectrumID")) %>% 
-    select(PeptideID, 
-           Sequence, 
-           Proteins, 
-           Area, 
-           Mass, 
-           m_z, 
-           Charge, 
-           Intensity, 
+  auc_table <- right_join(spectra, pep_table, by=c("SpectrumID" = "SpectrumID")) %>%
+    select(PeptideID,
+           Sequence,
+           Proteins,
+           Area,
+           Mass,
+           m_z,
+           Charge,
+           Intensity,
            FirstScan,
            SpectrumID)
-  
+
   return(auc_table)
-  
+
 }
--- R/make_pep_table.R
+++ R/make_pep_table.R
@@ -1,63 +1,64 @@
 
-#' Make a table of peptides
+#' Make a data frame of peptides from a thermo MSF file
 #'
-#' @param msf_file 
-#' @param min_conf 
+#' @param msf_file A file path to a thermo MSF file.
+#' @param min_conf "High", "Medium", or "Low". The mininum peptide confidence level to retrieve from MSF file.
 #'
-#' @return
+#' @return A data frame of all peptides above the confidence cut-off from a thermo MSF file.
 #' @export
 #'
 #' @examples
+#' make_pep_table("mythermofile.msf")
 make_pep_table <- function(msf_file, min_conf = "High") {
-  
+
   confidence <- switch(min_conf,
                        High = 3,
                        Medium = 2,
                        Low = 1,
                        3)
-  
+
   # Access MSF database file
   my_db <- src_sqlite(msf_file)
-  
+
   # This table maps PeptideIDs to ProteinIDs
   PeptidesProteins <- tbl(my_db, "PeptidesProteins") %>%
     select(PeptideID, ProteinID)
-  
+
   # Here are the actual peptide sequences with corresponding SpectrumIDs
   Peptides <- tbl(my_db, "Peptides") %>%
     select(PeptideID, SpectrumID, ConfidenceLevel, Sequence) %>%
     filter(ConfidenceLevel >= confidence)
-  
+
   # Protein descriptions are contained in this table
   ProteinAnnotations <- tbl(my_db, "ProteinAnnotations") %>%
     select(ProteinID, Description)
-  
+
   # Build a peptide table
-  pep_table <- inner_join(PeptidesProteins, Peptides, by = c("PeptideID" = "PeptideID")) %>% 
+  pep_table <- inner_join(PeptidesProteins, Peptides, by = c("PeptideID" = "PeptideID")) %>%
     inner_join(ProteinAnnotations, by = "ProteinID") %>%
     collect() %>%
     mutate(Proteins = str_match(Description, "^>([a-zA-Z0-9._]+)\\b")[,2]) %>%
     group_by(PeptideID) %>%
-    summarize(SpectrumID = unique(SpectrumID), 
-              Sequence = unique(Sequence), 
+    summarize(SpectrumID = unique(SpectrumID),
+              Sequence = unique(Sequence),
               Proteins = paste(Proteins, collapse = "; "))
-  
+
   # Append custom fields
   CustomFields <- tbl(my_db, "CustomDataFields")
-  
+
   # Grab custom peptide data using SQL because of automatic sqlite typing
   # This assumes that FieldValue will always be a number
   CustomPeptides <- tbl(my_db, sql("SELECT FieldID, PeptideID, CAST(FieldValue as REAL) AS FieldValue FROM CustomDataPeptides"))
-  
+
   # Spread custom fields as separate columns
   custom_data <- left_join(CustomPeptides, CustomFields, by = "FieldID") %>%
     select(PeptideID, DisplayName, FieldValue) %>%
     collect() %>%
     spread(DisplayName, FieldValue)
-  
+
   # Join to peptide table
   pep_table <- left_join(pep_table, custom_data, by = "PeptideID")
-  
+
   return(pep_table)
 
 }
